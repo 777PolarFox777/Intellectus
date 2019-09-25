6746587ec280d98ed706e6f94ea9c5eb75889ee6
STAJLb <RISEZID123>
2019-09-25 14:30:05 +0300

    Create server and first route

--- server/src/index.js
+++ server/src/index.js
@@ -3,10 +3,10 @@ const path = require('path');
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, './public')));
+app.use(express.static(path.join(__dirname, '../../client')));
 
 app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, './public', 'index.html'));
+  res.sendFile(path.join(__dirname, '../../client', 'index.html'));
 });
 
 app.listen(9000);
